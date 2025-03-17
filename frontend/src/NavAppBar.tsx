307562e7e22fad78ae16f96d44e39020751419b0
macy <macyeskelsen@gmail.com>
2025-03-17 14:22:27 -0600

    view the pages!

--- frontend/src/NavAppBar.tsx
+++ frontend/src/NavAppBar.tsx
@@ -1,43 +1,39 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-// import icons
-import homeBtn from "./assets/home.png";
-import calendarBtn from "./assets/calendar.png";
-import statsBtn from "./assets/stats.png";
-import recommendBtn from "./assets/recommend.png";
-import profileBtn from "./assets/profile.png";
+
 
 // Import pages
 import Home from "./pages/Home";
-import Stats from "./pages/Stats";
+import Stats from "./components/TempleDashboard";
 import Learn from "./pages/Learn";
-import Profile from "./pages/Profile";
-import Scheduling from "./pages/Scheduling";
+import Profile from "./components/MembershipCard";
+import Scheduling from "./components/TempleScheduler";
 import Login from "./pages/Login";
+import Welcome from "./components/MainLayout";
 
 const NavAppBar = () => {
   return (
     <BrowserRouter>
       <nav className="nav-bar">
         <Link to="/" className="nav-item">
-          <img className="icon" src={homeBtn} alt="Home" />
+          <img className="icon" src="./assets/home.png" alt="Home" />
           <span>Home</span>
         </Link>
         <Link to="/scheduling" className="nav-item">
-          <img className="icon" src={calendarBtn} alt="Calendar" />
+          <img className="icon" src="./assets/calendar.png" alt="Calendar" />
           <span>Scheduling</span>
         </Link>
         <Link to="/stats" className="nav-item">
-          <img className="icon" src={statsBtn} alt="Stats" />
+          <img className="icon" src="./assets/stats.png" alt="Stats" />
           <span>Stats</span>
         </Link>
         <Link to="/learn" className="nav-item">
-          <img className="icon" src={recommendBtn} alt="Learn" />
+          <img className="icon" src="./assets/recommend.png" alt="Learn" />
           <span>Learn</span>
         </Link>
         <Link to="/profile" className="nav-item">
-          <img className="icon" src={profileBtn} alt="Profile" />
+          <img className="icon" src="./assets/profile.png" alt="Profile" />
           <span>Profile</span>
         </Link>
         <Link to="/login" className="nav-item">
