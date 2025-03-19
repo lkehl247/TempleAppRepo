0e6b2e4571972270f720e1c1a177b4db6cb5fc38
Miles Blake <187061026+miles-blake@users.noreply.github.com>
2025-03-19 13:43:49 -0600

    Remove JavaScript files, keeping only TypeScript versions

--- frontend/src/components/StatCard.jsx
+++
@@ -1,6 +0,0 @@
-export const StatCard = ({ value, label }) => (
-  <article className="flex flex-col justify-center items-center p-5 h-40 rounded-xl border border-black">
-    <p className="mb-1 text-4xl font-bold">{value}</p>
-    <h3 className="text-base font-bold text-center">{label}</h3>
-  </article>
-);
