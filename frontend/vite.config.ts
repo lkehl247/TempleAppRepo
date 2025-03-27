a1804a89dc36aa72bb4d554328ccc3597d5467b8
macy <macyeskelsen@gmail.com>
2025-03-26 20:45:58 -0600

    making the port the frontend runs on standard

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,12 @@
 // @ts-ignore
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 // @ts-ignore
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+  },
+});
